ac2385a00a4702cda4429eb7c92c40064a8b443d
Abhishek Verma <www.abhiverma23@gmail.com>
2016-12-02 14:35:50 +0530

    First class room program

---
+++ firstProject/sampleDirectory/demo.py
@@ -0,0 +1,13 @@
+# Author : Abhishek Verma
+# E-Mail : abhishekverma3210@gmail.com
+# Date   : 1st Dec, 2016
+# Title  : First Program
+# Description : Creating variables and doing basic arithmetic operation.
+# Tested On   : python 3.5.0
+
+x = 1000
+y = 200
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
