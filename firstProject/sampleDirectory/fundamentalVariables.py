361f02ad592a609541fcda94ed31157b0ce97953
Abhishek Verma <www.abhiverma23@gmail.com>
2016-12-03 09:30:49 +0530

    2nd Dec, 2016 : Class examples

---
+++ firstProject/sampleDirectory/fundamentalVariables.py
@@ -0,0 +1,50 @@
+# Author : Abhishek Verma
+# E-Mail : abhishekverma3210@gmail.com
+# Date   : 2st Dec, 2016
+# Title  : Variables in python
+# Description : Fundamental variable uses and getting hashcode and type
+# Tested On   : python 3.5.0
+
+# Integer
+x = 1000
+print(x)
+print(id(x))
+print(type(x))
+
+x = 2000
+print(x)
+print(id(x))
+print(type(x))
+
+z = 2000
+print(z)
+print(id(z))
+print(type(z))
+
+# List
+x = [1000]
+print(x)
+print(id(x))
+print(type(x))
+
+z = [1000]
+print(z)
+print(id(z))
+print(type(z))
+
+y = x
+print(y)
+print(id(y))
+print(type(y))
+
+# Complex
+a = 3 + 4j
+print(a)
+print(id(a))
+print(type(a))
+
+# Boolean
+b = True
+print(b)
+print(id(b))
+print(type(b))
