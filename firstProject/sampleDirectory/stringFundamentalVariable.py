c492b52f89a7c7c83bc3c379bdab95ffa9d138bb
Abhishek Verma <www.abhiverma23@gmail.com>
2016-12-04 00:13:30 +0530

    2nd Dec, 2016 : Class examples : String

---
+++ firstProject/sampleDirectory/stringFundamentalVariable.py
@@ -0,0 +1,22 @@
+# Author : Abhishek Verma
+# E-Mail : abhishekverma3210@gmail.com
+# Date   : 3rd Dec, 2016
+# Title  : Fundamental Variable
+# Description : String fundamental variable and operations.
+# Tested On   : python 3.5.0
+
+x = 'abcdef'
+print(x)
+print(id(x))
+print(type(x))
+print(len(x))
+print(x[1])
+# print(x[7])
+print(x[-2])
+# print(x[-8])
+print(x[2:])
+print(x[2:5])
+print(x[-3:])
+print(x[-6:-2])
+print(x[-2:-6])
+print(x[3:1])
